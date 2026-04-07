923b1e9fb05a317bc6cfddfc13eb96d768dddcd6
AggroSec <btmaides1019@gmail.com>
2026-04-07 11:26:49 -0400

    6. changed validation to clean the chirp of 'bad words'

--- validate_chirp.go
+++ validate_chirp.go
@@ -3,15 +3,12 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 func chirpValidationHandler(w http.ResponseWriter, r *http.Request) {
 	type Chirp struct {
 		Body string `json:"body"`
-	}
-
-	type ValidResponse struct {
-		Valid bool `json:"valid"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -27,5 +24,28 @@ func chirpValidationHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	respondWithJSON(w, 200, ValidResponse{Valid: true})
+	chirp.Body = cleanBody(chirp.Body)
+
+	respondWithJSON(w, 200, map[string]string{"cleaned_body": chirp.Body})
+}
+
+func cleanBody(body string) string {
+	split_body := strings.Split(body, " ")
+	bad_words := map[string]bool{
+		"kerfuffle": true,
+		"sharbert":  true,
+		"fornax":    true,
+	}
+	clean_body_slice := []string{}
+
+	for _, word := range split_body {
+		lowerWord := strings.ToLower(word)
+		if bad_words[lowerWord] {
+			clean_body_slice = append(clean_body_slice, "****")
+		} else {
+			clean_body_slice = append(clean_body_slice, word)
+		}
+	}
+	clean_body := strings.Join(clean_body_slice, " ")
+	return clean_body
 }
